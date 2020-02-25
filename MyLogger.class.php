72bf3acd28d95520ab94c1d88183390e4dc869d1
redlolz <robinlego@hotmail.nl>
2020-02-25 11:29:04 +0100

    Uitwerking van de exercise

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -1,43 +1,43 @@
 <?php
 class MyLogger {
-	private function logWithTime($message) {
-		// [2019-11-27 15:30]
-		return date('[Y-m-d H:i] ') . $message;
-	}
-	public function warning($message) {
-		echo $this->logWithTime('WARNING: ' . $message . PHP_EOL);
-	}	
-	
-	public function error($message) {
-		echo $this->logWithTime('ERROR: ' . $message . PHP_EOL);
-	}
+    private function logWithTime($message) {
+        return date('[Y-m-d H:i] ') . $message;
+    }
+    public function warning($message) {
+        echo $this->logWithTime('WARNING: ' . $message . PHP_EOL);
+    }	
 
-	public function debug($message) {
-		echo $this->logWithTime('DEBUG: ' . $message . PHP_EOL);
-	}	
-	
-	public function info($message) {
-		echo $this->logWithTime('INFO: ' . $message . PHP_EOL);
-	}
+    public function error($message) {
+        echo $this->logWithTime('ERROR: ' . $message . PHP_EOL);
+    }
 
-	public function log($message, $loglevel = "") {
-		switch ($loglevel) {
-			case 'WARNING':
-				$this->warning($message);
-				break;
-			case 'ERROR':
-				$this->error($message);
-				break;
-			case 'DEBUG':
-				$this->debug($message);
-				break;
-			case 'INFO':
-				$this->info($message);
-				break;
-			default:
-				echo $message;
-		}
-	}
+    public function debug($message) {
+        echo $this->logWithTime('DEBUG: ' . $message . PHP_EOL);
+    }
+
+    public function info($message) {
+        echo $this->logWithTime('INFO: ' . $message . PHP_EOL);
+    }
+
+    public function log($message, $loglevel = "") {
+        switch ($loglevel) {
+            case 'WARNING':
+                $this->warning($message);
+                break;
+            case 'ERROR':
+                $this->error($message);
+                break;
+            case 'DEBUG':
+                $this->debug($message);
+                break;
+            case 'INFO':
+                $this->info($message);
+                break;
+            default:
+                echo $message;
+                break;
+        }
+    }
 }
 
 $logger = new MyLogger();
